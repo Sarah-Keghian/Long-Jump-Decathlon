3ef43ca75e1e17282a37a6be2edeb1eb08260ea2
Arnaud-Serres <a.serres04@gmail.com>
2024-06-25 15:11:36 +0200

    Update front
    Initialisation du score, les dés fonctionnent

--- long-jump-front-end/script.js
+++ long-jump-front-end/script.js
@@ -1,12 +1,12 @@
 const bouton_lancer = document.getElementById("bouton_lancer");
 const bouton_saut = document.getElementById("bouton_saut");
-const dice1 = document.getElementById("dice1");
+let dans_run_up = true
 
 bouton_lancer.addEventListener("click", function fetch_lancer() {
         fetch('https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json')
-            .then(response => response.text())
+            .then(response => response.json())
             .then(response => {
-                affiche_content(response)
+                affiche_content(response["members"][0]["age"])
             })
             .catch(error => {
                 console.log("Il y a eu un problème avec l'opération fetch: " + error.message);
@@ -14,21 +14,31 @@ bouton_lancer.addEventListener("click", function fetch_lancer() {
 });
 
 
+// depend de l'info donnée : texte = [valeur_de_1, valeur_de_2, valeur_de_3, valeur_de_4, valeur_de_5]
 function affiche_content(texte) {
-    var conteneur = document.getElementById('content');
-    var nouveauParagraphe = document.createElement("p");
-    nouveauParagraphe.appendChild(document.createTextNode(texte))
-    conteneur.appendChild(nouveauParagraphe);
+    document.getElementById('dice1').textContent = texte;
 }
 
-
 bouton_saut.addEventListener("click", function() {
-    
     bouton_saut.classList.add('invisible');
+    dans_run_up = false
 });
 
-dice1.addEventListener("click", function() {
-    
-    dice1.classList.add('invisible');
+document.querySelectorAll(".dice").forEach(dice => {
+    dice.addEventListener("click", function() {
+        const conteneur_actif = document.getElementById("actif");
+        const conteneur_gele = document.getElementById("gele");
+        if (conteneur_actif.contains(dice)) {
+            conteneur_actif.removeChild(dice);
+            conteneur_gele.appendChild(dice);
+        };
+        if (dans_run_up) {
+            if (document.getElementById('score_run_up').textContent == 0) {
+                document.getElementById('score_run_up').textContent = parseInt(dice.textContent)
+            }
+            else {
+                document.getElementById('score_run_up').textContent = parseInt(document.getElementById('score_run_up').textContent) + parseInt(dice.textContent)
+            }
+        };
+    });
 });
-
