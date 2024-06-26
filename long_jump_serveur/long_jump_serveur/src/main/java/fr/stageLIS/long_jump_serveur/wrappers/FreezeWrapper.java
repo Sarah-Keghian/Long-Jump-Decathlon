0cfeb7e228a9e1b3e3f1968076e6b37dd072e2c7
Arnaud-Serres <a.serres04@gmail.com>
2024-06-26 15:36:34 +0200

    chelou

---
+++ long_jump_serveur/long_jump_serveur/src/main/java/fr/stageLIS/long_jump_serveur/wrappers/FreezeWrapper.java
@@ -0,0 +1,14 @@
+package fr.stageLIS.long_jump_serveur.wrappers;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class FreezeWrapper {
+
+    private Long id;
+    private Long idDe;
+}
