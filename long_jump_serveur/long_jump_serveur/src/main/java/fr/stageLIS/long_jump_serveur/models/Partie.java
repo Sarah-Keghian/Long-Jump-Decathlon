23dbc9fdd23f574b2a99ac404cf287966b1e8712
Sarah Keghian <keghian.sarah@gmail.com>
2024-06-26 10:03:25 +0200

    Création Data Partie

---
+++ long_jump_serveur/long_jump_serveur/src/main/java/fr/stageLIS/long_jump_serveur/models/Partie.java
@@ -0,0 +1,25 @@
+package fr.stageLIS.long_jump_serveur.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Setter
+@Getter
+@Builder
+public class Partie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private int id;
+
+    private int etape;
+}
+
