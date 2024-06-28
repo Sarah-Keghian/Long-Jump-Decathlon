eef857e195ea6a12c59a9ea122501ccd954bf0ae
Arnaud-Serres <a.serres04@gmail.com>
2024-06-28 09:13:24 +0200

    Update script

--- long_jump_serveur/long_jump_serveur/src/main/resources/static/script.js
+++ long_jump_serveur/long_jump_serveur/src/main/resources/static/script.js
@@ -58,7 +58,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 })
                     .then(response => response.json())
                     .then(response => {
-                        id_essai = response
+                        id_essai = response["id"]
                         console.log("id_essai", id_essai)
                     })
                     .catch(error => {
@@ -226,12 +226,13 @@ document.addEventListener("DOMContentLoaded", function() {
 
     function reset_game() {
         const score_jump = parseInt(document.getElementById('score_jump').textContent)
+        console.log(id_essai, score_jump)
         fetch('/api/Essais/addEssai', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({"id": id_essai, "objet": score_jump})
+            body: JSON.stringify({"id": id_essai, "score": score_jump})
         })
             .then(response => {
                 if (!response.ok) {
